ee849a62ab5ec61b72acc6ad5f8ddd58a401f7d8
Mitchell Pontague <mitchell.pontague@LHGroup.local>
2016-08-23 15:22:32 +0800

    Updates for marketing and to camera updating

--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -1,36 +1,20 @@
 $(document).ready(function ($) {
   var internal_showing = $('#internal-1');
-  // var internal_working = $('#internal-2');
-  // var external_showing = $('#external-1');
-  // var external_working = $('#external-2');
+  var timeout;
+  var timestamp = Date.now();
+  $('#internal-1').bind('load', update_internal_image);
+
   update_internal_image();
-  // update_external_image();
-  setInterval(function () {
-    update_internal_image();
-    // update_external_image();
-  }, 1000);
 
   function update_internal_image() {
-    // var oldshowing = internal_showing;
-    // internal_showing = internal_working;
-    // internal_working = oldshowing;
-    // internal_showing.css('visibility', 'visible');
-    // internal_working.css('visibility', 'hidden');
-
-    // internal_working.attr('src', '/api/camera/internal?' + Date.now());
-    // $("#internal-2").css('margin-top', '-' + internal_showing.height() + 'px').css('display', 'block');
-
-    internal_showing.attr('src', '/api/camera/internal?' + Date.now());
+    if (timestamp + 1000 <= Date.now()) {
+      internal_showing.attr('src', '/api/camera/internal?' + Math.floor(Date.now() / 1000));
+      clearTimeout(timeout);
+      timeout = setTimeout(update_internal_image, 20000);
+      timestamp = Date.now();
+    }
+    else {
+      setTimeout(update_internal_image, Date.now - timestamp + 1000);
+    }
   }
-
-  // function update_external_image() {
-  //   var oldshowing = external_showing;
-  //   external_showing = external_working;
-  //   external_working = oldshowing;
-  //   external_showing.css('visibility', 'visible');
-  //   external_working.css('visibility', 'hidden');
-
-  //   external_working.attr('src', '/api/camera/external?' + Date.now());
-  //   $("#external-2").css('margin-top', '-' + external_showing.height() + 'px').css('display', 'block');
-  // }
 });
