d528def3dce3d48ec773040e9e739c8760898835
mitchellp <mitchell.pontague@gmail.com>
2018-03-23 13:06:26 +0800

    calsses

--- public/js/battmon.js
+++ public/js/battmon.js
@@ -4,13 +4,15 @@ function updateBattmon(data) {
   for (var i = 0; i < data.Bank.length; i++) {
     $('.string-' + (i + 1) + ' .battery-temp .temperature')[0].childNodes[0].nodeValue = data.Bank[i].Temperature.value;
 
-    $('.string-' + (i + 1) + ' .battery-1 .voltage')
-    [0].childNodes[0].nodeValue = data.Bank[i].Battery[0].status == 1 ? 'Alarm High' : data.Bank[n].Battery[0].status == -1 ? 'Alarm Low' : 'Good';
-    $('.string-' + (i + 1) + ' .battery-2 .voltage')
-    [0].childNodes[0].nodeValue = data.Bank[i].Battery[1].status == 1 ? 'Alarm High' : data.Bank[n].Battery[0].status == -1 ? 'Alarm Low' : 'Good';
-    $('.string-' + (i + 1) + ' .battery-3 .voltage')
-    [0].childNodes[0].nodeValue = data.Bank[i].Battery[2].status == 1 ? 'Alarm High' : data.Bank[n].Battery[0].status == -1 ? 'Alarm Low' : 'Good';
-    $('.string-' + (i + 1) + ' .battery-4 .voltage')
-    [0].childNodes[0].nodeValue = data.Bank[i].Battery[3].status == 1 ? 'Alarm High' : data.Bank[n].Battery[0].status == -1 ? 'Alarm Low' : 'Good';
+    for (var j = 0; j < 4; j++) {
+      $('.string-' + (i + 1) + ' .battery-' + (j + 1)).removeClass('batt-allow');
+      $('.string-' + (i + 1) + ' .battery-' + (j + 1)).removeClass('batt-alhigh');
+
+      $('.string-' + (i + 1) + ' .battery-' + (j + 1))
+          .addClass(battmon.Bank[i].Battery[j].status == 1 ? 'batt-alhigh' : battmon.Bank[i].Battery[j].status == -1 ? 'batt-allow' : '')
+
+      $('.string-' + (i + 1) + ' .battery-' + (j + 1) + ' .voltage')
+      [0].childNodes[0].nodeValue = data.Bank[i].Battery[j].status == 1 ? '⇡ High' : data.Bank[n].Battery[0].status == -1 ? '⇣ Low' : '✔ Good';
+    }
   }
 }
