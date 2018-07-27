bf2e4c223a37391c7abc3d6acfe4712a1a8985fa
Mitchell Pontague <mitchell.pontague@gmail.com>
2018-07-27 08:26:25 +0800

    Revert battmon from yes no to values

--- routes/battmon.js
+++ routes/battmon.js
@@ -1,3 +1,25 @@
+// var express = require('express');
+// var hostdiscovery = require('../hostdiscovery');
+// var jumpers = require('../jumpers');
+// var battmon_polling = require('../battmon_polling');
+// var alias = require('../alias');
+// var router = express.Router();
+
+// console.log("battmon route loaded");
+
+// router.get('/', function (req, res, next) {
+//   var data = {};
+//   data['alias'] = alias.alias;
+//   data['localize'] = jumpers.localize;
+//   data['battmon'] = battmon_polling.data;
+//   data['hosts'] = hostdiscovery.hosts_data;
+//   data['battmon_strings'] = jumpers.battmon_strings;
+
+//   res.render('battmon', data);
+// });
+
+// module.exports = router;
+
 var express = require('express');
 var hostdiscovery = require('../hostdiscovery');
 var jumpers = require('../jumpers');
@@ -7,15 +29,15 @@ var router = express.Router();
 
 console.log("battmon route loaded");
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   var data = {};
   data['alias'] = alias.alias;
   data['localize'] = jumpers.localize;
-  data['battmon'] = battmon_polling.data;
+  data['battmon'] = battmon_polling.medians;
   data['hosts'] = hostdiscovery.hosts_data;
   data['battmon_strings'] = jumpers.battmon_strings;
 
-  res.render('battmon', data);
+  res.render('test', data);
 });
 
 module.exports = router;
