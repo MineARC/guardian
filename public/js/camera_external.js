577b398fefe9ca18bd7c6bda2333b557deb90b1f
Mitchell Pontague <mitchell.pontague@gmail.com>
2019-08-23 08:34:57 +0800

    Fix checkbox checking, increase camera time

--- public/js/camera_external.js
+++ public/js/camera_external.js
@@ -1,20 +1,20 @@
 $(document).ready(function ($) {
   var external_showing = $('#external');
   var timeout;
-  var timestamp = Date.now() - 1001;
+  var timestamp = Date.now() - 10001;
   $('#external').bind('load', update_external_image);
 
   update_external_image();
 
   function update_external_image() {
-    if ($('#enableCameras').val() === 'on' && timestamp + 1000 <= Date.now()) {
-      external_showing.attr('src', '/api/camera/external?' + Math.floor(Date.now() / 1000));
+    if ($('#enableCameras').prop('checked') && timestamp + 10000 <= Date.now()) {
+      external_showing.attr('src', '/api/camera/external?' + Math.floor(Date.now() / 10000));
       clearTimeout(timeout);
       timeout = setTimeout(update_external_image, 20000);
       timestamp = Date.now();
     }
     else {
-      setTimeout(update_external_image, Date.now - timestamp + 1000);
+      setTimeout(update_external_image, Date.now - timestamp + 10000);
     }
   }
 });
