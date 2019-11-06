369c7327fc9f1e32a0cbaa5a3a4bf91015e2bea0
Mitchell Pontague <mitchell.pontague@gmail.com>
2019-11-06 10:42:45 +0800

    Overhaul app startup to delay for jumpers

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var auth = require('basic-auth');
-var compression = require('compression');
-var cors = require('cors');
 var AutoUpdater = require('auto-updater');
 var jumpers = require('./jumpers');
+
+// Export empty app
+var app = express();
+module.exports = app;
 
 var autoupdater = new AutoUpdater({
   pathToJson: '',
@@ -20,60 +16,70 @@ var autoupdater = new AutoUpdater({
   devmode: false
 });
 
-// State the events 
-autoupdater.on('git-clone', function () {
-  console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
+// State the events
+autoupdater.on('git-clone', function() {
+  console.log(
+      'You have a clone of the repository. Use \'git pull\' to be up-to-date');
 });
-autoupdater.on('check.up-to-date', function (v) {
-  console.info("You have the latest version: " + v);
+autoupdater.on('check.up-to-date', function(v) {
+  console.info('You have the latest version: ' + v);
 });
-autoupdater.on('check.out-dated', function (v_old, v) {
-  console.warn("Your version is outdated. " + v_old + " of " + v);
-  autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually. 
-  // Maybe ask if the'd like to download the update. 
+autoupdater.on('check.out-dated', function(v_old, v) {
+  console.warn('Your version is outdated. ' + v_old + ' of ' + v);
+  autoupdater.fire('download-update');  // If autoupdate: false, you'll have to
+                                        // do this manually.
+  // Maybe ask if the'd like to download the update.
 });
-autoupdater.on('update.downloaded', function () {
-  console.log("Update downloaded and ready for install");
-  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
+autoupdater.on('update.downloaded', function() {
+  console.log('Update downloaded and ready for install');
+  autoupdater.fire(
+      'extract');  // If autoupdate: false, you'll have to do this manually.
 });
-autoupdater.on('update.not-installed', function () {
-  console.log("The Update was already in your folder! It's read for install");
-  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
+autoupdater.on('update.not-installed', function() {
+  console.log('The Update was already in your folder! It\'s read for install');
+  autoupdater.fire(
+      'extract');  // If autoupdate: false, you'll have to do this manually.
 });
-autoupdater.on('update.extracted', function () {
-  console.log("Update extracted successfully!");
-  console.warn("RESTART THE APP!");
+autoupdater.on('update.extracted', function() {
+  console.log('Update extracted successfully!');
+  console.warn('RESTART THE APP!');
   process.exit();
 });
-autoupdater.on('download.start', function (name) {
-  console.log("Starting downloading: " + name);
+autoupdater.on('download.start', function(name) {
+  console.log('Starting downloading: ' + name);
 });
-autoupdater.on('download.progress', function (name, perc) {
-  process.stdout.write("Downloading " + perc + "% \033[0G");
+autoupdater.on('download.progress', function(name, perc) {
+  process.stdout.write('Downloading ' + perc + '% \033[0G');
 });
-autoupdater.on('download.end', function (name) {
-  console.log("Downloaded " + name);
+autoupdater.on('download.end', function(name) {
+  console.log('Downloaded ' + name);
 });
-autoupdater.on('download.error', function (err) {
-  console.error("Error when downloading: " + err);
+autoupdater.on('download.error', function(err) {
+  console.error('Error when downloading: ' + err);
 });
-autoupdater.on('end', function () {
-  console.log("The app is ready to function");
+autoupdater.on('end', function() {
+  console.log('The app is ready to function');
 });
-autoupdater.on('error', function (name, e) {
+autoupdater.on('error', function(name, e) {
   console.error(name, e);
 });
 
 // Start checking
-require('dns').resolve('github.com', function (err) {
-  if (!err)
-    autoupdater.fire('check');
+require('dns').resolve('github.com', function(err) {
+  if (!err) autoupdater.fire('check');
 });
 
 setTimeout(guardian, 10000);
 
 function guardian() {
-
+  var path = require('path');
+  var favicon = require('serve-favicon');
+  var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+  var auth = require('basic-auth');
+  var compression = require('compression');
+  var cors = require('cors');
   var dashboard = require('./routes/overview');
   var chamber = require('./routes/home');
   if (jumpers.mode == 0) var elv = require('./routes/elv');
@@ -95,23 +101,23 @@ function guardian() {
   if (jumpers.mode == 2) var series3_polling = require('./series3_polling');
   if (jumpers.mode == 3) var series4_polling = require('./series4_polling');
   if (jumpers.cams) var cams_polling = require('./cams_polling');
-  if (jumpers.aura) var aura_polling = require('./aura_polling')
+  if (jumpers.aura) var aura_polling = require('./aura_polling');
   if (jumpers.firefly) var firefly = require('./firefly');
   var alarms_polling = require('./alarms_polling');
   var hostdiscovery = require('./hostdiscovery');
 
-  var app = express();
 
   app.use(cors());
   app.options('*', cors());
 
   app.use(compression())
 
-  var admins = { 'username': { password: 'password' } };
+  var admins = {'username': {password: 'password'}};
 
   // app.use(function (req, res, next) {
   //   var user = auth(req);
-  //   if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+  //   if (!user || !admins[user.name] || admins[user.name].password !==
+  //   user.pass) {
   //     res.set('WWW-Authenticate', 'Basic realm="example"');
   //     return res.status(401).send();
   //   }
@@ -126,14 +132,13 @@ function guardian() {
   app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({extended: false}));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
 
   // Add firefly to request
   app.use('*', (req, res, next) => {
-    if(jumpers.firefly)
-      res.locals.firefly = firefly;
+    if (jumpers.firefly) res.locals.firefly = firefly;
     next();
   });
 
@@ -157,7 +162,7 @@ function guardian() {
   app.use('/contact', contact);
 
   // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
+  app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -168,21 +173,17 @@ function guardian() {
   // development error handler
   // will print stacktrace
   if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
       res.status(err.status || 500);
-      res.render('error', {
-        error: err
-      });
+      res.render('error', {error: err});
     });
   }
 
   // production error handler
   // no stacktraces leaked to user
-  app.use(function (err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      error: {}
-    });
+    res.render('error', {error: {}});
   });
 
   module.exports = app;
